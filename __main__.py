61cc83f88cf4004bc155c20867b4f16152b7ccbc
Ondrej Spanik <spanik11@msn.com>
2021-10-17 19:33:17 +0200

    Removed proof, added side length input check

--- __main__.py
+++ __main__.py
@@ -4,34 +4,23 @@ from _state import *
 # later will be user input
 n = 3 # 8-puzzle side length
 init = State([3,2,8,4,5,6,7,1,0]) # initial state, 0 => empty field
-init_impostor = State([3,2,8,4,5,6,7,1,0])
-init_then_down = State([3,2,8,4,5,0,7,1,6])
-init_then_down_again = State([3,2,8,4,5,0,7,1,6])
 fin = State([1,2,3,4,5,6,7,8,0]) # final state
 
 # existing states that if reached again won't be considered by the algorithm
 # no need to include the final state, because once it will be reached the algorithm will end successfuly
 states = set() 
 states.add(init) # todo move this addition after the state has been expanded with operators into a tree
-print(f"This should say true: {init_impostor in states}")
-print(f"This should say false: {init_then_down in states}")
-print(f"This should also say false: {init_then_down_again in states}")
-states.add(init_then_down) # todo move this addition after the state has been expanded with operators into a tree
-print(f"This should now say true: {init_then_down in states}")
-print(f"This should now also say true: {init_then_down_again in states}")
-print(f"There should be two states in the set: {len(states)}")
-states.add(fin) 
-states.add(fin) 
-states.add(fin) 
-states.add(fin) 
-print(f"Now I tried to add the same state a number of times, but it should only be once in the set, meaning {len(states)} has to be three")
 
 # number pair check (if there is a lone number in one of the user input states then they can't be accepted)
 assert (len(init) == len(fin)), "Initial and final states must have the same element count"
+init_count = 0
 for i in init.elms:
+    init_count += 1
     found = False
     for j in fin.elms:
         if (i == j): 
             found = True
             break
     assert (found == True), f"An element '{i}' is missing from the final state."
+# side length is not part of the state and is simply checked ahead to speed things up
+assert (init_count == n * n), f"The number of elms in a state has to be n * n, otherwise it wouldn't be much of a side length, now would it?"
--- _state.py
+++ _state.py
@@ -3,9 +3,6 @@
 class State(object):
 
     def __init__(self, _elms: tuple): 
-        # state elms must not be changed, that's why tuple (which is an immutable list)
-        self.elms = _elms
-        
         # create a hash from a tuple
         # hash is needed for set()
         
@@ -15,15 +12,23 @@ class State(object):
         h = ""
         for e in _elms:
             h += str(e) + "|"
+
+        # actually properly generate and save the hash
         self._elms_hash = hash(h)
+
+        # state elms must not be changed, that's why tuple (which is an immutable list)
+        self.elms = _elms
 
         return
 
     def __len__(self):
+        # there isn't much else to measure here than the element count, might as well make it direct to the class
         return len(self.elms)
 
     def __hash__(self):
+        # for set()
         return self._elms_hash
 
     def __eq__(self, _state: object) -> bool:
+        # for set()
         return False if not isinstance(_state, State) else self.elms == _state.elms
