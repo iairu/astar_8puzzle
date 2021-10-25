42cce5bc07a687e09ab8d952d9884653e8042049
Ondrej Spanik <spanik11@msn.com>
2021-10-25 20:58:46 +0200

    User input implemented in main

--- __main__.py
+++ __main__.py
@@ -1,37 +1,92 @@
 from _state import *
 from _alg import *
+import sys
+import readchar
 
-# todo: add proper user input (either args or better scanf python thing)
+def in_sidelen():
+    # Side length user input
+    n = 0
+    try:
+        n = int(input("Enter side length of ?-puzzle (3 for 8-puzzle because 3x3=9): ")) # 8-puzzle side length
+    except:
+        print("Error: Invalid value, enter a number!")
+        sys.exit()
+    return n
 
-# later will be user input
-n = 3 # 8-puzzle side length
-init = State(n,[3,2,8,4,5,6,7,1,0]) # initial state, 0 => empty field
-fin = State(n,[1,2,3,4,5,6,7,8,0]) # final state
+def in_elms(sidelen: int, state_name: str = ""):
+    # Elements
+    traversed = []
+    count = sidelen * sidelen
 
-init2 = State(n,[5,0,8,4,2,1,7,3,6]) # initial state, 0 => empty field # todo later: delete, temp additional print check
-fin2 = State(n,[1,2,3,4,5,6,7,8,0]) # final state # todo later: delete, temp additional print check
+    print(state_name + " state - Input " + str(count) + " elements, use 0 for an empty one.")
+    print("Use spaces for separation of elements, hit Enter when done:")
 
-init3 = State(n,[1,2,0,4,5,3,7,8,6]) # initial state, 0 => empty field # todo later: delete, temp additional print check
-fin3 = State(n,[1,2,3,4,5,6,7,8,0]) # final state # todo later: delete, temp additional print check
+    x = input() # input() can't be limited to single char
+    rawelms = x.split(" ") # space separated input
 
-# number pair check (if there is a lone number in one of the user input states then they can't be accepted)
-assert (len(init) == len(fin)), "Initial and final states must have the same element count"
-init_count = 0
-found_zero = False
-for i in init.elms:
-    init_count += 1
-    found = False
-    for j in fin.elms:
-        if (i == j): 
-            found = True
-            if (i == 0):
-                found_zero = True
-            break
-    assert (found == True), f"An element '{i}' is missing from the final state."
-assert (found_zero == True), f"An element '0' (representing an empty field) must be included in both states."
-# side length is not part of the state and is simply checked ahead to speed things up
-assert (init_count == n * n), f"The number of elms in a state has to be n * n, otherwise it wouldn't be much of a side length, now would it?"
+    # check every element for validity
+    i = 0
+    for i,e in enumerate(rawelms):
+        try:
+            elm = int(e)
+        except:
+            print("Error: Invalid value, enter numbers! Separate with spaces!")
+            sys.exit()
+        if (elm < 0 or elm > count - 1):
+                print("Error: Invalid element (must be <0;" + str(count - 1) + ">, but found " + str(elm) + ")")
+                sys.exit()
+        if (i > count - 1):
+                print("Error: Too many elements!")
+                sys.exit()
+        if (elm in traversed):
+            print("Error: Duplicate input for element " + str(elm))
+            sys.exit()
+        traversed.append(elm)
+    if (i != count - 1):
+        print("Error: Not enough elements! Only entered " + str(i + 1))
+        sys.exit()
 
-AStar.explore(init, fin)
-AStar.explore(init2, fin2)
-AStar.explore(init3, fin3)
+    return traversed
+
+def in_rec_depth(default_rec_depth: int):
+    # Recursion depth
+    try:
+        rd = input("Enter recursion depth or leave empty for default (" + str(default_rec_depth) + "): ")
+    except:
+        print("Error: Invalid value, enter a number or nothing!")
+        sys.exit()
+
+    if (rd == ""):
+        out = default_rec_depth
+    else:
+        out = int(rd)
+        if (out < -1):
+            print("Error: Invalid value, enter a number!")
+            sys.exit()
+    return out
+
+
+# ---------------------------------
+
+def main():
+
+    n = in_sidelen()
+    init = State(n, in_elms(n, "Initial"))
+    fin = State(n, in_elms(n, "Final"))
+    AStar.explore(init, fin, in_rec_depth(1000))
+
+    # init1 = State(n,[3,2,8,4,5,6,7,1,0])
+    # fin1 = State(n,[1,2,3,4,5,6,7,8,0])
+
+    # init2 = State(n,[5,0,8,4,2,1,7,3,6])
+    # fin2 = State(n,[1,2,3,4,5,6,7,8,0])
+
+    # init3 = State(n,[1,2,0,4,5,3,7,8,6])
+    # fin3 = State(n,[1,2,3,4,5,6,7,8,0])
+
+    # AStar.explore(init1, fin1)
+    # AStar.explore(init2, fin2)
+    # AStar.explore(init3, fin3)
+
+if __name__ == "__main__":
+    main()
